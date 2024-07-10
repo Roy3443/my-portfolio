8855cd5568ca4e2bdfe805e8115f6b6a5a5d42d8
Roy3443 <21b43.roy@sjec.ac.in>
2024-07-10 07:22:58 +0530

    complete about component

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,35 +2,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div
-      name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
-    >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+    <div id="about" className="w-full pt-14 bg-black text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white">
+        <div>
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
-
-        <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        <p className="text-xl py-3">
+          Hello, I'm Roy Joshua Lasrado, from Mangaluru, Karnataka, India. Currently in my third year of Computer Science and Engineering at St Joseph Engineering College, I am driven by a deep passion for technology and a strong desire to share knowledge in my journey as an aspiring engineer.
         </p>
-
         <br />
-
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          Beyond technology, I have a deep affection for plants and nature. I find solace in nurturing greenery and appreciate the tranquility they bring to spaces. This love for plants underscores my belief in sustainability and drives me to seek harmony between technological advancement and environmental stewardship.
         </p>
       </div>
     </div>
